6b73e9541fa92e7f176ee61161d2e9059f48bc72
Ibrohim Begaliev <81876891+ibegaliev@users.noreply.github.com>
2023-04-18 21:49:50 +0500

    change

--- Fitness Tracker App/Controllers/TabbarController.swift
+++ Fitness Tracker App/Controllers/TabbarController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TabbarController.swift
 //  Fitness Tracker App
 //
 //  Created by ibrohim begaliev on 4/18/23.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class TabbarController: UITabBarController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
 }
-
